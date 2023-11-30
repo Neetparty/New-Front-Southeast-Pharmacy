a395a562d36d6ba3ca3573cdc13c79807cc72401
sathapatk <sathapat.k@cloudnatician.com>
2023-11-30 18:16:22 +0700

    Fix index item loading

--- app/GlobalRedux/provider.tsx
+++ app/GlobalRedux/provider.tsx
@@ -5,15 +5,19 @@ import { store } from './store';
 import { I18nextProvider } from 'react-i18next'
 import i18n from '@/translate/i18n';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Suspense } from 'react';
+import { Circular } from '../component/Loading/Circular';
 const clientId = process.env.NEXT_PUBLIC_APP_GOOGLE_OAUTH
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <Provider store={store}>
             <I18nextProvider i18n={i18n}>
-                <GoogleOAuthProvider clientId={clientId as string}>
-                    {children}
-                </GoogleOAuthProvider>
+                <Suspense fallback={<Circular loading />}>
+                    <GoogleOAuthProvider clientId={clientId as string}>
+                        {children}
+                    </GoogleOAuthProvider>
+                </Suspense>
             </I18nextProvider>
         </Provider>
     )
